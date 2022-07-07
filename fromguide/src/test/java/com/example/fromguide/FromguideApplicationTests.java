981885ad15fdeed6b2dc7ea05c7d4a6645f4243c
mr20ac <98917163+mr20ac@users.noreply.github.com>
2022-07-06 18:07:43 -0700

    Add files via upload

---
+++ fromguide/src/test/java/com/example/fromguide/FromguideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fromguide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FromguideApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
